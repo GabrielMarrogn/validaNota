83eefb67bec39e4e1fe90e0ffa530fdd6c14c2bb
GabrielMarrogn <gabrielosantos2012@hotmail.com>
2024-01-09 21:05:38 -0300

    second

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Faça um programa que leia as notas referentes às duas avaliações de um aluno. Calcule e imprima a média semestral. Faça com que o algoritmo só aceite notas válidas (uma nota válida deve pertencer ao intervalo [0,10]). Cada nota deve ser validada separadamente.
+Escreva um programa para ler as notas da primeira e a segunda avaliação de um aluno. Calcule e imprima a média semestral. O programa só deverá aceitar notas válidas (uma nota válida deve pertencer ao intervalo [0,10]). Cada nota deve ser validada separadamente.
+
+No final deve ser impressa a mensagem “novo calculo (1-sim 2-nao)”, solicitando ao usuário que informe um código (1 ou 2) indicando se ele deseja ou não executar o algoritmo novamente, (aceitar apenas os código 1 ou 2). Se for informado o código 1 deve ser repetida a execução de todo o programa para permitir um novo cálculo, caso contrário o programa deve ser encerrado.
--- src/App.java
+++ src/App.java
@@ -3,20 +3,43 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
+        int qtd = 0;
+        int novo = 0;
         double media = 0;
 
-        for(int i =0; i <2;){
+        for (;;) {
             double nota = sc.nextDouble();
-            if(nota <0 || nota > 10){
+            if (nota < 0 || nota > 10) {
                 System.out.println("nota invalida");
-            }else if(nota >= 0 && nota <=10 ){
+            } else if (nota >= 0 && nota <= 10) {
                 media = media + nota;
-                i++;
+                qtd++;
+            }
 
+            if (qtd == 2) {
+                media = media / 2;
+                System.out.printf("media = %.2f\n", media);
+
+                for (;;) {
+                    System.out.println("novo calculo (1-sim 2-nao)");
+                    novo = sc.nextInt();
+                    if (novo == 2 || novo == 1) {
+                        break;
+                    }
+                }
+
+                if (novo == 2) {
+                    break;
+                }
+
+                if(novo == 1){
+                    qtd = 0;
+                    media = 0;
+                }
             }
         }
-        media = media/2;
-        System.out.printf("media = %.2f\n", media);
-        sc.next();
+
+        sc.close();
     }
+
 }
